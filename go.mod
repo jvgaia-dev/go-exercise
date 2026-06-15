7a90e7c6c8caf74dafb62a8104092fa7c0977849
jvgaia-dev <jvgaia30@gmail.com>
2026-06-14 22:20:45 -0300

    values & types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module exemple.com/investment-calculator 
+
+go 1.26.2
--- nivel-2/exercicio1/exercicio1.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    // Exercício 1
-	x := 100
-    fmt.Printf("%d, %x, %b", x, x, x)
-}
