f69430602fdb76739d40f3afdc102257fecc05ab
jvgaia-dev <jvgaia30@gmail.com>
2026-06-23 20:40:12 -0300

    add exercise upgrades

--- Exercise01/profit_calculator.go
+++ Exercise01/profit_calculator.go
@@ -5,27 +5,40 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	//var revenue float64
+	//var expenses float64
+	//var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	//fmt.Print("Revenue: ")
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Percentage of Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	//fmt.Print("Expenses: ")
+	//fmt.Scan(&expenses)
 
-	fmt.Print("Percentage of Tax Rate: ")
-	fmt.Scan(&taxRate)
-	taxRate = taxRate / 100
+	//fmt.Print("Percentage of Tax Rate: ")
+	//fmt.Scan(&taxRate)
 
-	earningsBeforeTax := revenue - expenses
-	taxAmount := earningsBeforeTax * taxRate
-	earningsAfterTax := earningsBeforeTax - taxAmount
-	ratio := earningsBeforeTax / earningsAfterTax
+	ebt, profit, ratio := taxCalculate(revenue, expenses, taxRate)
 
 	fmt.Println("-------------")
-	fmt.Println("Earnings before tax: ", earningsBeforeTax)
-	fmt.Println("Earnings after tax: ", earningsAfterTax)
+	fmt.Println("Earnings before tax: ", ebt)
+	fmt.Println("Earnings after tax: ", profit)
 	fmt.Println("Ratio: ", ratio)
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func taxCalculate(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	return ebt, profit, ratio
 }
