548e16111c822b2567d64d0f8e2cabbcc446c861
jvgaia-dev <jvgaia30@gmail.com>
2026-06-26 23:27:06 -0300

    add operations to the bank

--- Exercise02/bank.go
+++ Exercise02/bank.go
@@ -1,10 +1,12 @@
-package exercise02
+package main
 
 import (
 	"fmt"
 )
 
 func main() {
+	var accountBalance float64 = 1000
+
 	fmt.Println("Welcome to Go Bank")
 	fmt.Println("What you want to do?")
 	fmt.Println("1. Check balance")
@@ -13,8 +15,17 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var choice int
-	fmt.Println("Your choice: ")
+	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choice: ", choice)
+	if choice == 1 {
+		fmt.Println(accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount //accountBalance = accountBalance + depositAmount
+
+		fmt.Print("balance updated! new amount: ", accountBalance)
+	}
 }
