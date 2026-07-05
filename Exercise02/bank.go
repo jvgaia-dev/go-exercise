bc60571421e54c9ed37ccf44431651c6410a22ea
João Victor <jvgaia30@gmail.com>
2026-07-05 11:46:55 -0300

    add loop structure

--- Exercise02/bank.go
+++ Exercise02/bank.go
@@ -7,51 +7,53 @@ import (
 func main() {
 	var accountBalance float64 = 1000
 
-	fmt.Println("Welcome to Go Bank")
-	fmt.Println("What you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposity money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+	for i := 0; i < 200; i++ {
+		fmt.Println("Welcome to Go Bank")
+		fmt.Println("What you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposity money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println(accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
+		if choice == 1 {
+			fmt.Println(accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
 
-		if depositAmount <= 0 {
-			fmt.Println("Invalid Amount. Must be greater then 0.")
-			return
+			if depositAmount <= 0 {
+				fmt.Println("Invalid Amount. Must be greater then 0.")
+				return
+			}
+
+			accountBalance += depositAmount //accountBalance = accountBalance + depositAmount
+
+			fmt.Println("balance updated! new amount: ", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("Withdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid Withdraw. Must be greater then 0.")
+				return
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Insufficient funds")
+				return
+			}
+
+			accountBalance -= withdrawAmount //accountBalance = accountBalance - withdrawAmount
+
+			fmt.Println("balance updated! new amount: ", accountBalance)
+		} else {
+			fmt.Println("Exiting...")
 		}
-
-		accountBalance += depositAmount //accountBalance = accountBalance + depositAmount
-
-		fmt.Println("balance updated! new amount: ", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Withdraw amount: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid Withdraw. Must be greater then 0.")
-			return
-		}
-
-		if withdrawAmount > accountBalance {
-			fmt.Println("Insufficient funds")
-			return
-		}
-
-		accountBalance -= withdrawAmount //accountBalance = accountBalance - withdrawAmount
-
-		fmt.Println("balance updated! new amount: ", accountBalance)
-	} else {
-		fmt.Println("Exiting...")
 	}
 }
